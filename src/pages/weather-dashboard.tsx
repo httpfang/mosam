d565e01773d3e32a3559e1ec92460c77bbcae09b
Harsh verma <hv72002@gmail.com>
2024-12-01 21:47:33 +0530

    current location funcationality is completed

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { AlertTriangle, MapPin, RefreshCcw } from 'lucide-react'
+import { useGeolocation } from '@/hooks/use-geolocation'
+import WeatherSkeleton from '@/components/loading-skeleton';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 
-const WeatherDashboard = () => {
+
+
+export function WeatherDashboard() {
+  const {
+    coordinates,
+    error: locationError,
+    isLoading: locationLoading,
+    getLocation,
+  } = useGeolocation();
+
+
+const handleRefresh = () => {
+  getLocation();
+  if (coordinates) {
+    // weatherQuery.refetch();
+    // forecastQuery.refetch();
+    // locationQuery.refetch();
+  }
+};
+
+if (locationLoading) {
+  return <WeatherSkeleton />;
+}
+
+if (locationError) {
   return (
-    <div>
-      wd
+    <Alert variant="destructive">
+      <AlertTriangle className="h-4 w-4" />
+      <AlertTitle>Location Error</AlertTitle>
+      <AlertDescription className="flex flex-col gap-4">
+        <p>{locationError}</p>
+        <Button variant="outline" onClick={getLocation} className="w-fit">
+          <MapPin className="mr-2 h-4 w-4" />
+          Enable Location
+        </Button>
+      </AlertDescription>
+    </Alert>
+  );
+}
+  return (
+    <div className='space-y-4'>
+      <div className='flex items-center justify-between'>
+        <h1 className='text-xl font-bold tracking-tight'>My Location</h1>
+        <Button variant={'outline'} size={'icon'} onClick={handleRefresh}>
+          <RefreshCcw className='h-4 w-4' />
+        </Button>
+      </div>
+
     </div>
   )
 }
